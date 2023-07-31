0b4f926811d265ebba6ff0dc99529e0c7f034e37
thesua7 <sams.ahamed@northsouth.edu>
2023-07-31 13:31:52 +0600

    Singleton Implemented

--- src/SingleTon/Singleton.java
+++ src/SingleTon/Singleton.java
@@ -1,2 +1,18 @@
-package SingleTon;public class Singleton {
+package SingleTon;
+//Only can create one instance at a time
+//No multiple instance/obj of a class
+public class Singleton {
+static Singleton SingletonObj = new Singleton();
+private Singleton(){
+
+}
+
+public static Singleton getInstance(){
+    if (SingletonObj==null)
+        SingletonObj = new Singleton();
+    return SingletonObj;
+}
+
+
+
 }
